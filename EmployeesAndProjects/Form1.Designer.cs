dc18f5370fed9ad053a2ffebe489676c9a2b2dc7
kaloyancvetkov <kaloyancvetkov21@gmail.com>
2023-03-16 15:40:53 +0200

    Task solved

---
+++ EmployeesAndProjects/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace EmployeesAndProjects
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.EmployeesAndProjects = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.filePath = new System.Windows.Forms.TextBox();
+            this.Pairs = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.EmployeesAndProjects)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pairs)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // EmployeesAndProjects
+            // 
+            this.EmployeesAndProjects.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.EmployeesAndProjects.Location = new System.Drawing.Point(12, 90);
+            this.EmployeesAndProjects.Name = "EmployeesAndProjects";
+            this.EmployeesAndProjects.RowHeadersWidth = 51;
+            this.EmployeesAndProjects.RowTemplate.Height = 24;
+            this.EmployeesAndProjects.Size = new System.Drawing.Size(689, 425);
+            this.EmployeesAndProjects.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(557, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 44);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Parse and Display Data";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // filePath
+            // 
+            this.filePath.Location = new System.Drawing.Point(12, 49);
+            this.filePath.Name = "filePath";
+            this.filePath.Size = new System.Drawing.Size(527, 22);
+            this.filePath.TabIndex = 2;
+            // 
+            // Pairs
+            // 
+            this.Pairs.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Pairs.Location = new System.Drawing.Point(728, 96);
+            this.Pairs.Name = "Pairs";
+            this.Pairs.RowHeadersWidth = 51;
+            this.Pairs.RowTemplate.Height = 24;
+            this.Pairs.Size = new System.Drawing.Size(643, 419);
+            this.Pairs.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Myanmar Text", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(814, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(466, 63);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Pair of employees who have worked together on common project for the longest time" +
+    "";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1383, 541);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Pairs);
+            this.Controls.Add(this.filePath);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.EmployeesAndProjects);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.EmployeesAndProjects)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pairs)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView EmployeesAndProjects;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox filePath;
+        private System.Windows.Forms.DataGridView Pairs;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ EmployeesAndProjects/Form1.cs
@@ -0,0 +1,161 @@
+﻿namespace EmployeesAndProjects
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data;
+    using System.IO;
+    using System.Linq;
+    using System.Windows.Forms;
+    using System.Xml.Linq;
+
+    public partial class Form1 : Form
+    {
+        DataTable dt = new DataTable();
+        DataTable dt2 = new DataTable();
+        List<string> employees = new List<string>();
+
+        Dictionary<string, string[]> pairs = new Dictionary<string, string[]>();
+        Dictionary<string, int> daysTogether = new Dictionary<string, int>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog();
+            openFileDialog.ShowDialog();
+            filePath.Text = openFileDialog.FileName;
+            BindDataCSV(filePath.Text);
+
+            string[] headerLabels = new string[] { "Employee ID #1", "Employee ID #2", "Project ID", "Days worked" };
+
+            var kvp = GetPairs();
+            foreach (var headerWord in headerLabels)
+            {
+                dt2.Columns.Add(new DataColumn(headerWord));
+            }
+
+            DataRow row = dt2.NewRow();
+            int index = 0;
+
+            foreach (var headerWord in headerLabels)
+            {
+                row[headerWord] = kvp[index++];
+            }
+
+            dt2.Rows.Add(row);
+
+            if (dt2.Rows.Count > 0)
+            {
+                Pairs.DataSource = dt2;
+            }
+        }
+
+        private string[] GetPairs()
+        {
+            while (employees.Count > 0)
+            {
+                employees.ForEach((line1) =>
+                {
+                    var el1 = line1.Split(';');
+                    var emp1Id = el1[0];
+                    var projectId = el1[1];
+                    var emp1StartDate = DateTime.Parse(el1[2]);
+                    var emp1EndDate = el1[3];
+
+                    employees.GetRange(employees.IndexOf(line1) + 1, employees.Count - employees.IndexOf(line1) - 1).ForEach(line2 =>
+                    {
+                        var el2 = line2.Split(';');
+                        var emp2Id = el2[0];
+                        var emp2StartDate = DateTime.Parse(el2[2]);
+                        var emp2EndDate = el2[3];
+
+                        if (el1[0] != el2[0])
+                        {
+                            var startDate1 = emp1StartDate;
+                            var endDate1 = emp1EndDate == "NULL" ? new DateTime() : DateTime.Parse(emp1EndDate);
+                            var startDate2 = emp2StartDate;
+                            var endDate2 = emp2EndDate == "NULL" ? new DateTime() : DateTime.Parse(emp2EndDate);
+
+                            var start = DateTime.Compare(startDate1, startDate2) <= 0 ? startDate2 : startDate1;
+                            var end = DateTime.Compare(endDate1, endDate2) <= 0 ? endDate2 : endDate1;
+
+                            if (end >= startDate2)
+                            {
+                                var diffDays = Math.Abs(end.Subtract(start).Days);
+                                var x = $"{el1[0]}{el2[0]}";
+
+                                if (!daysTogether.ContainsKey(x))
+                                {
+                                    daysTogether[x] = 0;
+                                }
+                                daysTogether[x] = 1 * daysTogether[x] + diffDays;
+
+                                if (!pairs.ContainsKey(x))
+                                {
+                                    pairs[x] = new string[] { emp1Id, emp2Id, projectId, diffDays.ToString() };
+                                }
+                            }
+                        }
+                    });
+                });
+
+                return pairs[daysTogether.Keys.Aggregate((a, b) => daysTogether[a] > daysTogether[b] ? a : b)];
+
+            }
+
+            return null;
+        }
+
+        private void BindDataCSV(string filePath)
+        {
+            string[] lines = File.ReadAllLines(filePath);
+
+            if (lines.Length > 0) 
+            {
+                string firstLine = lines[0];
+
+                string[] headerLabels = firstLine.Split(';');
+
+                foreach (var headerWord in headerLabels)
+                {
+                    dt.Columns.Add(new DataColumn(headerWord));
+                }
+
+                for (int i = 1; i < lines.Length; i++)
+                {
+                    string[] dataElements = lines[i].Split(';');
+                    employees.Add(lines[i]);
+
+                    DataRow row = dt.NewRow();
+                    int columnIndex = 0;
+
+                    foreach (var headerWord in headerLabels)
+                    {
+                        row[headerWord] = dataElements[columnIndex++];
+                    }
+
+                    dt.Rows.Add(row);
+                }
+            }
+
+            if (dt.Rows.Count > 0)
+            {
+                EmployeesAndProjects.DataSource = dt;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (dt.Columns.Count > 0 && dt.Rows.Count > 0)
+            {
+                List<string> pairs = new List<string>();
+                List<int> daysTogether = new List<int>();
+            }
+
+            
+        }
+    }
+}
